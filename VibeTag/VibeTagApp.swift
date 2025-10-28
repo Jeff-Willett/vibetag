e8bda4ffe1086bccac3d78721edc59fa91adc523
Jeff Willett <jpwillett@gmail.com>
2025-10-28 16:27:16 -0500

    Initial Commit

---
+++ VibeTag/VibeTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeTagApp.swift
+//  VibeTag
+//
+//  Created by Jeff Willett on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct VibeTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
